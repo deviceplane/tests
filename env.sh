f81792949fdf222ebba6a2bab783756854adc7f2
sergerad <serge.radinovich@gmail.com>
2021-08-02 15:19:55 +1200

    Add project var

--- env.sh
+++ env.sh
@@ -2,10 +2,10 @@
 
 export TT_CONTROLLER_BASE_URL="https://cloud.dev.deviceplane.com/api"
 export TT_PROJECT=""
-export TT_ACCESS_KEY=""
 
 ######## cli
 export TT_CLI_VERSION=""
+export TT_ACCESS_KEY=""
 ######################################################################
 
 ######## agent
