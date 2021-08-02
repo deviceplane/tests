c7e7856e203cb4fb7afd98d192cde97973613ab9
sergerad <serge.radinovich@gmail.com>
2021-08-02 13:35:39 +1200

    Sudo install

--- func/test.bash
+++ func/test.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 function testInstallCLI(){
-  wget -O /usr/local/bin/deviceplane https://downloads.deviceplane.com/cli/latest/darwin/amd64/deviceplane
-  chmod +x /usr/local/bin/deviceplane
+  sudo wget -O /usr/local/bin/deviceplane https://downloads.deviceplane.com/cli/latest/darwin/amd64/deviceplane
+  sudo chmod +x /usr/local/bin/deviceplane
   which deviceplane
   deviceplane --version
   deviceplane --help
