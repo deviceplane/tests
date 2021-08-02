34717b179be50b0ee3379cf83065540a068c3b91
sergerad <serge.radinovich@gmail.com>
2021-08-02 13:39:11 +1200

    Fix url

--- func/test.bash
+++ func/test.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 function testInstallCLI(){
-  sudo wget -O /usr/local/bin/deviceplane https://downloads.deviceplane.com/cli/latest/darwin/amd64/deviceplane
+  os=$(uname -s | tr '[:upper:]' '[:lower:]')
+  sudo wget -O "/usr/local/bin/deviceplane https://downloads.deviceplane.com/cli/${CLI_VERSION}/${os}/amd64/deviceplane"
   sudo chmod +x /usr/local/bin/deviceplane
   which deviceplane
   deviceplane --version
