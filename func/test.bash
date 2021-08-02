d5fce0d504db57d2e884139f44f33b2bafd95505
sergerad <serge.radinovich@gmail.com>
2021-08-02 13:40:06 +1200

    Fix typo

--- func/test.bash
+++ func/test.bash
@@ -2,7 +2,7 @@
 
 function testInstallCLI(){
   os=$(uname -s | tr '[:upper:]' '[:lower:]')
-  sudo wget -O "/usr/local/bin/deviceplane https://downloads.deviceplane.com/cli/${CLI_VERSION}/${os}/amd64/deviceplane"
+  sudo wget -O /usr/local/bin/deviceplane "https://downloads.deviceplane.com/cli/${CLI_VERSION}/${os}/amd64/deviceplane"
   sudo chmod +x /usr/local/bin/deviceplane
   which deviceplane
   deviceplane --version
